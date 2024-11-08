b983524dccfc5c94641745bf6a37f06a6da23b7b
mikedevv <maungdevv@gmail.com>
2024-11-08 16:26:44 +0700

    fix: add jsonwebtoken

--- controllers/TelegramController.js
+++ controllers/TelegramController.js
@@ -1,9 +1,17 @@
 
-
 const callback = async(req, res) => {
+    const { id, first_name, last_name, username, photo_url, auth_date, hash } = req.query;
     res.json({
         success: true,
-        message: 'telegram login success!'
+        message: 'telegram login success!',
+        data: {
+            first_name,
+            last_name,
+            username,
+            photo_url,
+            auth_date,
+            hash,
+        }
     })
 }
 
